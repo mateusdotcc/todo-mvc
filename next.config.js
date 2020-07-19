a48bbee217fe14e408c55f6bf84a13e10a273ef3
Mateus Henrique <hi@mateus.cc>
2020-07-19 11:36:07 -0300

    fix: build prod

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ module.exports = {
     { dev, dir, outDir, distDir, buildId },
   ) {
     return {
-      '/': { page: '/home' },
-      '/:id': { page: '/Home' },
+      '/': { page: '/' },
+      '/:id': { page: '/' },
     };
   },
   webpack(config) {
